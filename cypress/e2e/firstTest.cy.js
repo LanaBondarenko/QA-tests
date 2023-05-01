11b0ce52648eb34812f3deabb840bd59a392c028
LanaBondarenko <bondarenkosvitlana22@gmail.com>
2023-05-01 14:46:58 -0400

    first test commit

---
+++ cypress/e2e/firstTest.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress"/>
+describe ('My first test',() => {
+    beforeEach(()=> {
+        cy.visit("https://example.cypress.io")
+    })
+    //test case
+    it("check link", () => {
+        cy.visit("https://example.cypress.io")
+        cy.contains("clear").click()
+        // assertion
+        cy.url().should("include", "/commands/actions")
+    })
+    it("check link", () => {
+        
+        cy.contains("select").click()
+        // assertion
+        cy.url().should("include", "/commands/actions")
+        cy.get('div.container h1').should('have.text', 'Actions')
+    })
+})
